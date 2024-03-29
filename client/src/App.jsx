47e827541a47d4e501531b17436593770202f3e8
DelvonRH <delvonrowley@gmail.com>
2024-03-29 18:03:55 -0400

    Installed react router dom as well as created a folder to hold pages also added multiple pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 function App() {
-  return <main className="text-red-600 text-3xl">Hello</main>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
