0f743545e092c8adaf9d58047b60fe4e6fa40c60
DelvonRH <delvonrowley@gmail.com>
2024-03-29 21:30:39 -0400

    Created a user model

--- api/index.js
+++ api/index.js
@@ -15,6 +15,6 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000!!");
+app.listen(process.env.PORT_CONNECTION, () => {
+  console.log(`Server is running on port ${process.env.PORT_CONNECTION}!`);
 });
