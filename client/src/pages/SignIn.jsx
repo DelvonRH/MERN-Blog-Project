9438975792624d79d697d00e41a67ff3c879ea89
DelvonRH <delvonrowley@gmail.com>
2024-04-02 21:02:54 -0400

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,18 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -15,25 +22,24 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields.");
+      return dispatch(signInFailure("Please fill out all fields"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if (data.success === false) return setErrorMessage(data.message);
-      setLoading(false);
+      if (data.success === false) dispatch(signInFailure(data.message));
+
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -91,7 +97,7 @@ export default function SignIn() {
             </Button>
           </form>
           <div className="flex gap-1 text-sm mt-5">
-            <span>Don't have an account?</span>
+            <span>Don&apos;t have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
