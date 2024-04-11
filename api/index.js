e7a076bdb3aaeae874e99a0ea672d5b36db47ef8
DelvonRH <delvonrowley@gmail.com>
2024-04-11 18:27:31 -0400

    added upadte user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -17,6 +18,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(process.env.PORT_CONNECTION, () => {
   console.log(`Server is running on port ${process.env.PORT_CONNECTION}!`);
